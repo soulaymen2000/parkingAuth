6417a4e10dca3370ea9c76c9db46d10f802ea92e
Soulaymen <omrani54174@gmail.com>
2024-11-17 13:06:40 +0100

    reservation btn

--- src/main/java/glsib/parkingauth/repositories/ZoneRepository.java
+++ src/main/java/glsib/parkingauth/repositories/ZoneRepository.java
@@ -3,4 +3,5 @@ import glsib.parkingauth.entities.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
+    Zone findByDescription(String description);
 }
